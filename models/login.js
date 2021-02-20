c7e09936fe9a240202b92e17edbe46a2246816c2
Young Ji Kim <youjmik@gmail.com>
2021-02-20 14:13:28 -0500

    Adding on Associates

--- models/login.js
+++ models/login.js
@@ -1,16 +1,19 @@
+const bcrypt = require("bcryptjs");
+
+
 module.exports = (sequelize, DataTypes) => {
     const Login = sequelize.define('Login', {
 
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
+        // username: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
 
-            validate: {
-                len: [1, 15],
+        //     validate: {
+        //         len: [1, 15],
 
-            },
+        //     },
 
-        },
+        // },
 
         email: {
             type: DataTypes.STRING,
@@ -26,13 +29,26 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false
         },
 
-
-        complete: {
-            type: DataTypes.BOOLEAN,
-
-            defaultValue: false,
-        },
-
     });
-    return Login;
-};
+
+    Login.associate = (models) => {
+        Login.hasMany(models.Billboard, {
+          onDelete: 'cascade',
+        })
+      }
+
+    User.prototype.validPassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+      };
+      // Hooks are automatic methods that run during various phases of the User Model lifecycle
+      // In this case, before a User is created, we will automatically hash their password
+      User.addHook("beforeCreate", user => {
+        user.password = bcrypt.hashSync(
+          user.password,
+          bcrypt.genSaltSync(10),
+          null
+        );
+      });
+      return Login;
+    };
+    
