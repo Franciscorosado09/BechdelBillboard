86896bc8ea5d61830c615b45632396a9ed41708d
Rachel Wanke <rkhorne09@gmail.com>
2021-02-20 13:06:18 -0500

    adding routes for movies database.

---
+++ routes/movies-routes.js
@@ -0,0 +1,53 @@
+const db = require('../models');
+
+// Routes
+module.exports = (app) => {
+
+  app.get('/api/movies', (req, res) => {
+    db.Movies.findAll({}).then((dbMovies) => res.json(dbMovies));
+  });
+
+
+  app.post('/api/movies', (req, res) => {
+    db.Movies.create({
+      title: req.body.title,
+      year: req.body.year,
+      director: req.body.director,
+      genre: req.body.description,
+      rating: req.body.rating,
+
+    })
+      .then((dbMovies) => res.json(dbMovies))
+      .catch((err) => res.json(err));
+  });
+
+
+  app.delete('/api/movies/:id', (req, res) => {
+
+    db.Movies.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }).then((dbMovies) => res.json(dbMovies));
+  });
+
+
+  app.put('/api/Movies', (req, res) => {
+    db.Todo.update(
+      {
+        title: req.body.title,
+        year: req.body.year,
+        director: req.body.director,
+        genre: req.body.description,
+        rating: req.body.rating,
+      },
+      {
+        where: {
+          id: req.body.id,
+        },
+      }
+    )
+      .then((dbMovies) => res.json(dbMovies))
+      .catch((err) => res.json(err));
+  });
+};
