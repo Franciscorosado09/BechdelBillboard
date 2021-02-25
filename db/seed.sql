0cbbd917612fa6456d79b7f072bde459bc2f93e1
Francisco Rosado <Franciscorosado09@gmail.com>
2021-02-25 11:47:09 -0500

    update information

--- db/seed.sql
+++
@@ -1,6 +0,0 @@
-USE bechdeltest;
-
-
-INSERT INTO Movies (title, director, year, genre, description) VALUES ('Frick and Frack', 'Johnnie Simpson', '2021', 'horror', 'A movie of pure hell and joy');
-INSERT INTO Movies (title, director, year, genre, description) VALUES ('Frack and Frick', 'Amir Simpson', '2021', 'romance', 'A movie of pure hell and joy and love');
-INSERT INTO Movies (title, director, year, genre, description) VALUES ('The Backend Queens', 'Jeezy and Wanke', '2021', 'comedy', 'A hilarious movie about killing the game.');
