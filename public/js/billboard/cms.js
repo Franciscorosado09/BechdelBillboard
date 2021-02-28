0e73ca64c04e6f9e853daf2e2e027007145b0f5c
Francisco Rosado <Franciscorosado09@gmail.com>
2021-02-28 16:34:08 -0500

    commiting to pull

--- public/js/billboard/cms.js
+++ public/js/billboard/cms.js
@@ -1,9 +1,7 @@
 // Helper functions to show/hide elements
-const show = (el) => {
-  el.style.display = 'block';
-};
-
-
+// const show = (el) => {
+//   el.style.display = 'block';
+// };
 
 // Wait for the DOM to completely load before we run our JS
 document.addEventListener('DOMContentLoaded', () => {
@@ -21,15 +19,11 @@ document.addEventListener('DOMContentLoaded', () => {
   let userId;
   let updating = false;
 
-  
-
   // Get billboard data for editing/adding
   const getbillboardData = (id, type) => {
-    console.log("I'm inside getBillboard")
     const queryUrl =
-      type === 'billboard' ? `/api/billboard/${id}`:
-      // `/api/user_data`;
-
+      type === 'billboard' ? `/api/billboard/${id}` :
+// : `/api/user/${id}`
     fetch(queryUrl, {
       method: 'GET',
       headers: {
@@ -40,34 +34,28 @@ document.addEventListener('DOMContentLoaded', () => {
       .then((data) => {
         if (data) {
           console.log('Success in getting billboard:', data);
-        
+
           // Populate the form for editing
           titleInput.value = data.title;
           postInput.value = data.post;
-          //userId = data.userId || data.id;
+          // userId = data.userId || data.id;
 
+          // We are updating
           updating = true;
-
         }
       })
       .catch((err) => console.error(err));
-
-      
   };
- 
-  
+
   // If billboard exists, grab the content of the billboard
   if (url.indexOf('?billboard_id=') !== -1) {
-    console.log('grabing edit')
     billboardId = url.split('=')[1];
     getbillboardData(billboardId, 'billboard');
-    console.log(billboardId)
   }
-  // // // Otherwise if we have an user_id in our url, preset the user select box to be our user
-  // else if (url.indexOf('?user_id=') !== -1) {
-  //   userId = url.split('=')[1];
-  // }
-  //getbillboardData(billboardId, 'billboard')
+  // Otherwise if we have an user_id in our url, preset the user select box to be our user
+  else if (url.indexOf('?user_id=') !== -1) {
+    userId = url.split('=')[1];
+  }
 
   // Event handler for when the billboard for is submitted
   const handleFormSubmit = (e) => {
@@ -77,7 +65,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if (
       !titleInput.value.trim() ||
       !postInput.value.trim() 
-      //!userSelect.value
+      // ||
+      // !userSelect.value
     ) {
       return;
     }
@@ -86,10 +75,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const newbillboard = {
       title: titleInput.value.trim(),
       post: postInput.value.trim(),
-      //userId: userSelect.value,
+      // userId: userSelect.value,
     };
-    
-    //submitbillboard(newbillboard);
+    submitbillboard(newbillboard);
 
     // Update a billboard if flag is true, otherwise submit a new one
     if (updating) {
@@ -115,7 +103,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }).then((response) => response.json())
       .then((data) => {
-        window.location.href = '/billboard.html';
+        // window.location.href = '/billboard';
 
         console.log('Success in submitting post:', data);
         console.log(JSON.stringify(billboard))
@@ -129,54 +117,50 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   // Render a list of users or redirect if no users
-  // const renderuserList = (data) => {
-  //   console.log('renderuserList -> data', data);
-  //   if (!data.length) {
-  //     window.location.href = '/users';
-  //   }
-  //   if (document.querySelector('.hidden')) {
-  //     show(document.querySelector('.hidden'));
-  //   }
+  const renderuserList = (data) => {
+    console.log('renderuserList -> data', data);
+    if (!data.length) {
+      window.location.href = '/users';
+    }
+    if (document.querySelector('.hidden')) {
+      show(document.querySelector('.hidden'));
+    }
 
-  //   const rowsToAdd = [];
+    const rowsToAdd = [];
 
-  //   data.forEach((user) => rowsToAdd.push(createuserRow(user)));
+    data.forEach((user) => rowsToAdd.push(createuserRow(user)));
 
-  //   userSelect.innerHTML = '';
-  //   console.log('renderuserList -> rowsToAdd', rowsToAdd);
-  //   console.log('userSelect', userSelect);
+    userSelect.innerHTML = '';
+    console.log('renderuserList -> rowsToAdd', rowsToAdd);
+    console.log('userSelect', userSelect);
 
-  //   rowsToAdd.forEach((row) => userSelect.append(row));
-  //   userSelect.value = userId;
-  // };
+    rowsToAdd.forEach((row) => userSelect.append(row));
+    userSelect.value = userId;
+  };
 
   // Build user dropdown
-  // const createuserRow = ({ id, email }) => {
-  //   const listOption = document.createElement('option');
-  //   listOption.value = id;
-  //   listOption.textContent = email;
-  //   return listOption;
-  // };
+  const createuserRow = ({ id, name }) => {
+    const listOption = document.createElement('option');
+    listOption.value = id;
+    listOption.textContent = name;
+    return listOption;
+  };
 
   // A function to get users and then call the render function
-  // const getusers = () => {
-    
-  //   fetch('api/user_data', {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     //  body: JSON.stringify(billboard)
-  //   })
-  //     .then((response) => response.json())
-  //    // .then((data) => renderuserList(data))
-  //     .catch((err) => console.error(err));
-      
-     
-  // };
+  const getusers = () => {
+    fetch('api/users', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => renderuserList(data))
+      .catch((err) => console.error(err));
+  };
 
-  // // Get the users, and their billboards
-  //getusers();
+  // Get the users, and their billboards
+  getusers();
 
   // Update a billboard then redirect to billboard
   const updatebillboard = (billboard) => {
@@ -188,11 +172,9 @@ document.addEventListener('DOMContentLoaded', () => {
       body: JSON.stringify(billboard),
     })
       .then(() => {
-        window.location.href = '/billboard.html';
+        window.location.href = '/billboard';
       })
       .catch((err) => console.error(err));
   };
-
- 
   
 });
